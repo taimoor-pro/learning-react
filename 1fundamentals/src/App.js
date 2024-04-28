9e7085f3794f9478a18384bd349276af59acbf7a
taimoor-pro <taimoorrnawaz@gmail.com>
2024-04-28 15:48:54 +0500

    chore: 01 Fundamentals in React

---
+++ 1fundamentals/src/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  // Destructing Array
+  let [advice, setAdvice] = useState("");
+  //  advice = "Taimoor"; // I'm capable to change or modified this variable
+
+  const [count, setCount] = useState(0);
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json(); // if you will not use await so it still in promise fulfilled, when use await so resolved
+    setAdvice(data?.slip?.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(() => {
+    // It run two times when i page reload how this handle in future we will discuss
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+// Second Component
+function Message(props) {
+  // it not a simple function it accept object
+  return (
+    <p>
+      Count of total Advice <strong>{props.count}</strong> Advices
+    </p>
+  );
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # react-course
 I will start BA course, means Beginner to Advance or Expert course for react developers.  
+
+
+REACT
+Part 01: Fundamentals
+Part 02: Intermediate
+Part 03: Advanced
+Part 04 Professional Dev
+
+
+# Part 01: Fundamentals:
+https://react.new/ -> Using Online Code SendBox without any signup!
+
+https://api.adviceslip.com/advice -> Test API url 
+
+Follow Taimoor Notes:
+	PDF
+Follow Taimoor Codes:
+	$ Github -> https://github.com/taimoor-pro/react-course.git
